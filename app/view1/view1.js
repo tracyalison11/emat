c12c940992ebf2223e3c39d75f718f890780371b
tracyalison11 <tracyak13@gmail.com>
2017-04-17 22:51:11 -0400

    Implemented bootstrap carousel and removed some code for angular UI carousel

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -8,56 +8,7 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope) {
-  $scope.myInterval = 5000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  var slides = $scope.slides = [];
-  var currIndex = 1;
 
-  $scope.addSlide = function() {
-    var imageName = 'image' + currIndex;
-    slides.push({
-      image: '/img/carousel-images/' + imageName + '.jpg',
-      id: currIndex++
-    });
-  };
-
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
-
-  for (var i = 1; i < 5; i++) {
-    $scope.addSlide();
-  }
-
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
-
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
-
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
+  $('#carousel').carousel();
 
 });
