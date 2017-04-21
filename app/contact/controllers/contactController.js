5b3b97e6b0036edcde970a28f9f496de4755f9b6
tracyalison11 <tracyak13@gmail.com>
2017-04-21 00:00:51 -0400

    Added ng-model to contact form

--- app/contact/controllers/contactController.js
+++ app/contact/controllers/contactController.js
@@ -1,5 +1,17 @@
 angular.module('contact')
 
-.controller('contactController', function() {
+.controller('contactController', function($scope) {
+
+  $scope.data = {
+    firstName: undefined,
+    lastName: undefined,
+    email: undefined,
+    phone: undefined,
+    message: undefined
+  };
+
+  $scope.submit = function() {
+    console.log($scope.data);
+  }
 
 });
