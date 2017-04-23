7124a61bf563efc4eb9cb5cd89d934ce828a8935
tracyalison11 <tracyak13@gmail.com>
2017-04-23 15:20:26 -0400

    Carousel restructure, CSS updates

--- app/home/controllers/homeController.js
+++ app/home/controllers/homeController.js
@@ -1,5 +1,17 @@
 angular.module('home')
+.controller('homeController', function($scope) {
 
-.controller('homeController', function() {
+  // Carousel Photos Array
+  $scope.photos = [
+    {path: '/img/carousel-images/image-1.jpg'},
+    {path: '/img/carousel-images/image-2.jpg'},
+    {path: '/img/carousel-images/image-3.jpg'},
+    {path: '/img/carousel-images/image-4.jpg'},
+    {path: '/img/carousel-images/image-5.jpg'},
+    {path: '/img/carousel-images/image-6.jpg'}
+  ];
+
+  // Call Carousel with unique ID
   $('#carousel').carousel();
+
 });
