09b665731f445d019e0b978736e47c26a56e1613
tracyalison11 <tracyak13@gmail.com>
2017-04-23 23:02:00 -0400

    Adding generic image, added more clients in controller

--- app/clients/controllers/clientsController.js
+++ app/clients/controllers/clientsController.js
@@ -1,46 +1,92 @@
 angular.module('clients')
 
 .controller('clientsController', function($scope) {
-
   $scope.clients = [
+    {
+      name: 'Meggitt Sensing Systems',
+      path: 'img/clients/engineering.png',
+      project: '777 HQ Calibration Test Stand',
+      description: 'Design and consruct test stand for ATP and CTP of hydraulic fluid level monitors'
+    },
+    {
+      name: 'OECO',
+      path: 'img/clients/oeco.jpg',
+      project: 'Generator Failure Investigation',
+      description: 'Include epoxy cure identified as root cause'
+    },
+    {
+      name: 'PTI Technologies',
+      path: 'img/clients/engineering.png',
+      project: 'Overseas 6-4 TI Source',
+      description: 'Qualify for hydraulic manifold producion for Airbus A360'
+    },
+    {
+      name: 'South Coast Water District',
+      path: 'img/clients/engineering.png',
+      project: 'Pump Shaft Failure Investigation',
+      description: 'Bad shaft design identified as root cause'
+    },
+    {
+      name: 'John Crane/Belfab',
+      path: 'img/clients/engineering.png',
+      project: 'Edge Wielded Metal Bellows',
+      description: 'Production trouble shooting and design evaluation'
+    },
+    {
+      name: 'DANAHER/Pacific Scientific',
+      path: 'img/clients/engineering.png',
+      project: 'Digital TCPS',
+      description: 'Design and build a digital temperature compensated pressure switch'
+    },
+    {
+      name: 'Lockheed Martin',
+      path: 'img/clients/lockheed.jpg',
+      project: 'Arsoc PPS Test Stand',
+      description: 'Reproduce and update the ARSOC test stand'
+    },
+    {
+      name: 'DirecTV',
+      path: 'img/clients/directv.jpg',
+      project: 'ODU Corrosion',
+      description: 'Evaluate the corrosion on outdoor units'
+    },
+    {
+      name: 'Setra Sensors',
+      path: 'img/clients/engineering.png',
+      project: 'Capacitance Pressure Monitors',
+      description: 'Identify root cause of product malfunctions'
+    },
+    {
+      name: 'Southern California Edison',
+      path: 'img/clients/engineering.png',
+      project: 'Water Tree Formation',
+      description: 'Identify root cause of product malfunctions'
+    },
     {
       name: 'Raytheon',
       path: 'img/clients/raytheon.jpg',
-      project: 'Example Project',
-      description: 'Example Description'
+      project: 'Paveway Pressure Source',
+      description: 'Design and build a pressure sensor that does not breach the vessel wall'
+    },
+    {
+      name: 'Wartsilla',
+      path: 'img/clients/engineering.png',
+      project: 'Fillet Weld Evaluation',
+      description: 'Interface between Wartsilla and vendor on fillet weld issue'
+    },
+    {
+      name: 'IRWD',
+      path: 'img/clients/engineering.png',
+      project: 'Brown Water Corrosion Study',
+      description: '1 year corrosion field study microbiological induced corrosion'
+    },
+    {
+      name: 'Wavestream',
+      path: 'img/clients/engineering.png',
+      project: 'Solder Joint Evaluation',
+      description: 'Classical analysis and finite element evaluation of solder joints'
     }
   ];
-
-  $('.no-touch .project-list li:nth-child(3), .no-touch .project-list li:nth-child(8)').hover(function(e) {
-  $(this).parents('.client-meta').next('.overflow-wrapper').find('img:nth-child(1)').toggleClass('show-image');
-});
-
-$('.no-touch .project-list li:nth-child(4), .no-touch .project-list li:nth-child(9)').hover(function(e) {
-  $(this).parents('.client-meta').next('.overflow-wrapper').find('img:nth-child(2)').toggleClass('show-image');
-});
-
-$('.no-touch .project-list li:nth-child(5), .no-touch .project-list li:nth-child(9)').hover(function(e) {
-  $(this).parents('.client-meta').next('.overflow-wrapper').find('img:nth-child(3)').toggleClass('show-image');
-});
-
-$('.no-touch .project-list li:nth-child(6), .no-touch .project-list li:nth-child(10)').hover(function(e) {
-  $(this).parents('.client-meta').next('.overflow-wrapper').find('img:nth-child(4)').toggleClass('show-image');
-});
-
-$('.no-touch .project-list li:nth-child(7), .no-touch .project-list li:nth-child(11)').hover(function(e) {
-  $(this).parents('.client-meta').next('.overflow-wrapper').find('img:nth-child(5)').toggleClass('show-image');
-});
-
-// Reset
-$('.touch .client-wrap').click(function(event){
-	  var target = $( event.target );
-	  if ( target.hasClass( "client-close" ) ) {
-		  $('.client-wrap div.client').addClass('reset');
-	  }
-	  else{
-		  $('.client-wrap div.client').removeClass('reset');
-	  }
-});
 
 // White BG for client top
 $('.no-touch .project-list li').hover(function(e) {
@@ -54,12 +100,6 @@ $('.no-touch .project-list li').hover(function(e) {
   img.onload = function() {
     img.removeAttribute('data-src');
   };
-});
-
-// For the demo
-$('.hover .toggle-switch').click(function(e) {
-  $('.hover .toggle-switch').toggleClass('switched-off');
-  $('.client-wrap').toggleClass('opacity-focus');
 });
 
 });
