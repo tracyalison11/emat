9a7c6c9a4f591db40a4a343ace92b166f54e972b
tracyalison11 <tracyak13@gmail.com>
2017-04-24 19:12:39 -0400

    Link indication on services page

--- app/app.js
+++ app/app.js
@@ -35,18 +35,44 @@ ematApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
       templateUrl: 'views/services/services.html',
     })
 
-    .state('services.services-list', {
-      url: '/services-list',
-      parent: 'services',
-      templateUrl: 'views/services/services-list.html',
-      controller: 'servicesController',
-    })
+    // Services Child States
 
-    .state('services.simple-beams', {
-      url: '/simple-beams',
-      parent: 'services',
-      templateUrl: 'views/services/simple-beams.html'
-    })
+      .state('services.services-list', {
+        url: '/list',
+        parent: 'services',
+        templateUrl: 'views/services/services-list.html',
+        controller: 'servicesController',
+      })
+
+      .state('services.fracture-mechanics', {
+        url: '/fracture-mechanics',
+        parent: 'services',
+        templateUrl: 'views/services/fracture-mechanics.html'
+      })
+
+      .state('services.simple-beams', {
+        url: '/simple-beams',
+        parent: 'services',
+        templateUrl: 'views/services/simple-beams.html'
+      })
+
+      .state('services.material-properties', {
+        url: '/material-properties',
+        parent: 'services',
+        templateUrl: 'views/services/material-properties.html'
+      })
+
+      .state('services.miscellaneous', {
+        url: '/miscellaneous',
+        parent: 'services',
+        templateUrl: 'views/services/miscellaneous.html'
+      })
+
+      .state('services.roark-plates', {
+        url: '/roark-plates',
+        parent: 'services',
+        templateUrl: 'views/services/roark-plates.html'
+      })
 
     .state('clients', {
       url: '/clients',
