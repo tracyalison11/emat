26f2677ac295d449d882e21ca702c84680bfc7a1
tracyalison11 <tracyak13@gmail.com>
2017-06-16 21:50:18 -0400

    Replaced contact form with email for now and changed color

--- app/contact/controllers/contactController.js
+++ app/contact/controllers/contactController.js
@@ -2,23 +2,23 @@ angular.module('contact')
 
 .controller('contactController', function($scope) {
 
-  $scope.data = {
-    fullName: undefined,
-    email: undefined,
-    phone: undefined,
-    message: undefined
-  };
+  // $scope.data = {
+  //   fullName: undefined,
+  //   email: undefined,
+  //   phone: undefined,
+  //   message: undefined
+  // };
 
   // parameters: service_id, template_id, template_parameters
-  emailjs.send("<YOUR SERVICE ID>","<contactus>",{name: "James", notes: "Check this out!"})
-  .then(function(response) {
-     console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
-  }, function(err) {
-     console.log("FAILED. error=", err);
-  });
+  // emailjs.send("<YOUR SERVICE ID>","<contactus>",{name: "James", notes: "Check this out!"})
+  // .then(function(response) {
+  //    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+  // }, function(err) {
+  //    console.log("FAILED. error=", err);
+  // });
 
-  $scope.submit = function() {
-    console.log($scope.data);
-  }
+  // $scope.submit = function() {
+  //   console.log($scope.data);
+  // }
 
 });
